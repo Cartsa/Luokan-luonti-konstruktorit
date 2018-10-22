79aae4100c9aed7513981e9329d5fd49795c641d
Kotilainen Carolus <ao166537@gradia.fi>
2018-10-22 12:45:55 +0300

    Tehtava12

--- Tehtävä 2/ElainLuokat/Kissa.cs
+++ Tehtävä 2/ElainLuokat/Kissa.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 namespace ElainLuokat
 {
 
 
 public class Kissa : Nisakkaat
 {
+    public List<Kissa> pennut = new List<Kissa>();
+        public Kissa Emo;
     public Kissa()
         {
-            
+
+        }
+    public Kissa(string u_nimi, int u_ika)
+        {
+            asetaElaimenNimi(u_nimi);
+            asetaElaimenIka(u_ika);
+        }
+        public Kissa(string u_nimi, int u_ika,Kissa u_emo)
+        {
+            asetaElaimenNimi(u_nimi);
+            asetaElaimenIka(u_ika);
+            asetaEmo(u_emo);
+        }
+        public void LisaaPentu(string u_pentunimi, int u_ika)
+        {
+            Emo = this;
+            pennut.Add(new Kissa(u_pentunimi,u_ika,Emo));
+        }
+    public Kissa palautaEmo()
+        {
+                return Emo;
+        }
+        public void asetaEmo(Kissa u_emo)
+        {
+            Emo = u_emo;
         }
     }
 }
---
+++ Tehtävät 8-12/Tehtava12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElainLuokat;
+
+namespace Tehtava12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kissa mirri = new Kissa();
+            mirri.asetaElaimenNimi("Mirri");
+            Kissa kissa = new Kissa();
+            kissa.asetaElaimenNimi("Mouru");
+            kissa.LisaaPentu("Miina",0);
+            kissa.LisaaPentu("Manu", 1);
+            kissa.LisaaPentu("Masa", 0);
+            mirri.LisaaPentu("Mikke", 3);
+            foreach (Kissa pentu in kissa.pennut)
+            {
+                Kissa Emo = pentu.palautaEmo();    
+                Console.WriteLine("Kissan pennun nimi on " + pentu.palautaElaimenNimi() + ", se on " + pentu.palautaElaimenIka() + " vuotta vanha ja sen emo on " + Emo.palautaElaimenNimi());
+            }
+            foreach (Kissa pentu in mirri.pennut)
+            {
+                Kissa Emo = pentu.palautaEmo();
+                Console.WriteLine("Kissan pennun nimi on " + pentu.palautaElaimenNimi() + ", se on " + pentu.palautaElaimenIka() + " vuotta vanha ja sen emo on " + Emo.palautaElaimenNimi());
+            }
+            Console.ReadKey();
+        }
+    }
+}
